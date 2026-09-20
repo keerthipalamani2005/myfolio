b8c88ba25e4312fe2426df53fb2b4dbecfc54464
keerthi palamani <palamanikeerthi4@gmail.com>
2026-09-20 15:36:17 +0530

    Configure GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/myfolio/',
   plugins: [react()],
   resolve: {
     alias: {
